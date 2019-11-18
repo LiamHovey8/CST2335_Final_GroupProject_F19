92ba0638af21c3d32c2604af0c8983747c27733d
thedo <thedopplereffect22@gmail.com>
2019-11-18 17:55:45 -0500

    milestone 1 final Liam

--- app/src/main/java/com/example/cst2335_final_groupproject_f19/RecipePage.java
+++ app/src/main/java/com/example/cst2335_final_groupproject_f19/RecipePage.java
@@ -1,6 +1,10 @@
 package com.example.cst2335_final_groupproject_f19;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -10,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -21,13 +26,43 @@ public class RecipePage extends AppCompatActivity {
     //will be used later don't worry
     int positionClicked =0;
     //a copy of the adapter
-    MyOwnAdapter listAdapter;
+    BaseAdapter listAdapter;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.page_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.recipe_page_menu_link:
+
+                break;
+            case R.id.car_charger_page_menu_link:
+                Intent goToCarChargerFinder = new Intent(RecipePage.this, CarChargerFinder.class);
+                RecipePage.this.startActivityForResult(goToCarChargerFinder, 10);
+                break;
+            case R.id.currency_exchange_page_menu_link:
+                Intent goToCurrencyExchange = new Intent(RecipePage.this, CurrencyExchange.class);
+                RecipePage.this.startActivityForResult(goToCurrencyExchange, 10);
+                break;
+            case R.id.news_page_menu_link:
+                Intent goToNewsPage = new Intent(RecipePage.this, NewsPage.class);
+                RecipePage.this.startActivityForResult(goToNewsPage, 10);
+                break;
+        }
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_list_page);
+        Toolbar tBar = (Toolbar)findViewById(R.id.navigation_toolbar);
+        setSupportActionBar(tBar);
         //find the progress bar
         ProgressBar progressBar = findViewById(R.id.progress_bar);
         //set the progress bar to be visible so that it can be shown as a place holder
@@ -38,7 +73,7 @@ public class RecipePage extends AppCompatActivity {
         recipeList.add(new Recipe("A Spicy Perspective","http://food2fork.com/view/aa60ec","Butter Chicken Sliders with Pickled Mango Slaw","http://www.aspicyperspective.com/2013/03/butter-chicken-sliders-big-land-olakes-giveaway.html","aa60ec","http://static.food2fork.com/IMG_35501180x1803eee.jpg",99.99888808120132,"http://www.aspicyperspective.com"));
         recipeList.add(new Recipe("BBC Good Food","http://food2fork.com/view/495802","Chicken cacciatore","http://www.bbcgoodfood.com/recipes/4251/chicken-cacciatore","495802","http://static.food2fork.com/4251_MEDIUM71f0.jpg",99.99999994031722,"http://www.bbcgoodfood.com"));
         //find the list view
-        ListView recipeListView =findViewById(R.id.the_list);
+        ListView recipeListView =findViewById(R.id.the_recipe_list);
         //use the adapter populate the list
         recipeListView.setAdapter(listAdapter=new MyOwnAdapter());
         //set Toasts and Snackbars to give information about the Recipe
@@ -73,10 +108,10 @@ public class RecipePage extends AppCompatActivity {
             if(old==null)
                 newView = getLayoutInflater().inflate(R.layout.recipe_list_row, null);
 
-            TextView RecipeTitle=newView.findViewById(R.id.title);
+            TextView RecipeTitle=newView.findViewById(R.id.recipe_title);
             Recipe thisRow=getItem(position);
 
-            RecipeTitle.setText("Title:" + thisRow.getTitle());
+            RecipeTitle.setText(new StringBuilder().append("@string/").append(thisRow.getTitle()).toString());
 
             //return the row:
             return newView;
